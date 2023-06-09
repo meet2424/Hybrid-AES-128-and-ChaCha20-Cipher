6e5508248030cdd545f267bb783c9f55ca2c9d64
Meet <meetp272003@gmail.com>
2023-06-09 12:54:11 +0530

    Add encrypted image code

--- final.py
+++ final.py
@@ -13,21 +13,7 @@ def encrypt(image, key):
 
     # Generate a random 128-bit IV for the block cipher mode
     iv = get_random_bytes(16)
-
-    # # Create AES cipher object with CBC mode and the generated IV
-    # cipher_aes = AES.new(key, AES.MODE_CBC, iv)
-
-    # # Pad the pixel data to the block size of AES
-    # padded_data = pad(pixel_data, AES.block_size)
-
-    # # Encrypt the padded data
-    # encrypted_data = cipher_aes.encrypt(padded_data)
-
-    # # Use encrypted data and key to encrypt the IV using ChaCha20 stream cipher
-    # cipher_chacha = ChaCha20.new(key=key[:32])
-    # encrypted_iv = cipher_chacha.encrypt(iv)
-
-    # Divide plaintext into blocks of 128 bits
+ 
     block_size = 16  # 16 bytes = 128 bits
     plaintext_blocks = [pixel_data[i:i+block_size] for i in range(0, len(pixel_data), block_size)]
 
@@ -39,12 +25,9 @@ def encrypt(image, key):
     for i in range(1, len(plaintext_blocks)):
         cipher = ChaCha20.new(key=key, nonce=encrypted_blocks[-1][:12])
         encrypted_blocks.append(cipher.encrypt(plaintext_blocks[i]))
-
-    # Combine encrypted ciphertext and IV
-
+ 
     # Combine encrypted IV and data
-    encrypted_image_data = iv + b''.join(encrypted_blocks) 
-    # print(encrypted_blocks)
+    encrypted_image_data = iv + b''.join(encrypted_blocks)  
 
     # Create a new PIL image from the encrypted data
     encrypted_image = Image.frombytes('RGB', image.size, encrypted_image_data)
@@ -53,15 +36,9 @@ def encrypt(image, key):
     encrypted_image.save(encrypted_image_path, format=image.format)
     print("Encrypted image saved as:", encrypted_image_path)
 
-    return  encrypted_image
+    return  encrypted_image_data
 
-def decrypt( encrypted_image, key,width, height):
-    # Convert the encrypted image to a NumPy array
-    encrypted_array = np.array(encrypted_image)
-
-    # Flatten the array and convert it to bytes
-    encrypted_image_data = encrypted_array.flatten().tobytes()
-
+def decrypt( encrypted_image_data, key,width, height): 
       # Divide encrypted pixels into blocks of 128 bits
     block_size = 16  # 16 bytes = 128 bits
     encrypted_blocks = [ encrypted_image_data[i:i+block_size] for i in range(0, len( encrypted_image_data), block_size)]
@@ -84,60 +61,25 @@ def decrypt( encrypted_image, key,width, height):
     image = Image.frombytes('RGB', (width, height), decrypted_pixels)
 
     return image
-    # # Split the encrypted data into IV and encrypted pixels
-    # encrypted_iv = encrypted_data[:16]
-    # encrypted_pixels = encrypted_data[16:]
-    #  # Divide encrypted pixels into blocks of 128 bits
-    # block_size = 16  # 16 bytes = 128 bits
-    # encrypted_blocks = [encrypted_pixels[i:i+block_size] for i in range(0, len(encrypted_pixels), block_size)]
 
-    # # Use key and encrypted IV to decrypt the IV using ChaCha20 stream cipher
-    # cipher_chacha = ChaCha20.new(key=key[:32])
-    # iv = cipher_chacha.decrypt(encrypted_iv)
 
-    # # Create AES cipher object with CBC mode and the decrypted IV
-    # cipher_aes = AES.new(key, AES.MODE_CBC, iv)
-
-    # # Decrypt the encrypted pixels
-    # decrypted_pixels = cipher_aes.decrypt(encrypted_blocks)
-
-    # # Remove the padding from the decrypted pixels
-    # unpadded_data = unpad(decrypted_pixels, AES.block_size)
-
-    # Create a new PIL image from the decrypted data
-    # decrypted_image = Image.frombytes('RGB', encrypted_image.size, decrypted_pixels)
-
-    # return decrypted_image
-
-key = get_random_bytes(32)  # 256-bit key for AES-256 and ChaCha20
+print("*****Image Encryption Using Hybrid AES-128 and ChaCha20 Cipher*****")
+key = get_random_bytes(32)  # 256-bit key
 
 # Open the original image
-original_image_path = "sample.jpeg"
+original_image_path = "test1.jpeg"
 image = Image.open(original_image_path)
 # Get the width and height of the image
-width, height = image.size
-print("Original Image:")
-# image.show()
+width, height = image.size 
 
+print("Encrypting Image...")
 # Encrypt the image
 encrypted_image = encrypt(image, key)
 
-# Display the encrypted image as pixels
-print("Encrypted Image (Pixels):")
-# encrypted_image.show()
-
-# # Save the encrypted image in the same format as the original image
-# encrypted_image_path = "encrypted_image.jpeg"
-# encrypted_image.save(encrypted_image_path, format=image.format)
-# print("Encrypted image saved as:", encrypted_image_path)
-
+print("Decrypting Image...")
 # Decrypt the encrypted image
 decrypted_image = decrypt(encrypted_image, key,width, height)
-
-# Display the decrypted image
-print("Decrypted Image:")
-# decrypted_image.show()
-
+ 
 # Save the decrypted image in the same format as the original image
 decrypted_image_path = "decrypted_image.jpeg"
 decrypted_image.save(decrypted_image_path, format=image.format)
--- project.py
+++ project.py
@@ -15,67 +15,110 @@ def encrypt(plaintext, key):
 
     # Use IV and key to encrypt first plaintext block using AES-128 in CBC mode
     cipher = AES.new(key, AES.MODE_CBC, iv)
-    encrypted_blocks = [cipher.encrypt(plaintext_blocks[0])]
+    encrypted_blocks = [cipher.encrypt(plaintext_blocks[0].encode())]
 
     # Use encrypted ciphertext and key to encrypt subsequent plaintext blocks using ChaCha20 stream cipher
     for i in range(1, len(plaintext_blocks)):
         cipher = ChaCha20.new(key=key, nonce=encrypted_blocks[-1][:12])
-        encrypted_blocks.append(cipher.encrypt(plaintext_blocks[i]))
+        encrypted_blocks.append(cipher.encrypt(plaintext_blocks[i].encode()))
 
     # Combine encrypted ciphertext and IV
     encrypted_message = iv + b''.join(encrypted_blocks) 
     return encrypted_message
 
-def decrypt(encrypted_message, key):
-    # Split encrypted message into IV and encrypted ciphertext
-    iv = encrypted_message[:16]
-    encrypted_ciphertext = encrypted_message[16:]
-
-    # Divide encrypted ciphertext into blocks of 128 bits
+def decrypt(encrypted_pixels, key,width, height):
+     # Divide encrypted pixels into blocks of 128 bits
     block_size = 16  # 16 bytes = 128 bits
-    encrypted_blocks = [encrypted_ciphertext[i:i+block_size] for i in range(0, len(encrypted_ciphertext), block_size)]
+    encrypted_blocks = [encrypted_pixels[i:i+block_size] for i in range(0, len(encrypted_pixels), block_size)]
 
-    # Use IV and key to decrypt first ciphertext block using AES-128 in CBC mode
+    # Split the first 16 bytes as IV for AES decryption
+    iv = encrypted_blocks.pop(0)
+
+    # Use IV and key to decrypt first pixel block using AES-128 in CBC mode
     cipher = AES.new(key, AES.MODE_CBC, iv)
     decrypted_blocks = [cipher.decrypt(encrypted_blocks[0])]
 
-    # Use decrypted plaintext and key to decrypt subsequent ciphertext blocks using ChaCha20 stream cipher
+    # Use decrypted pixels and key to decrypt subsequent blocks using ChaCha20 stream cipher
     for i in range(1, len(encrypted_blocks)):
         cipher = ChaCha20.new(key=key, nonce=encrypted_blocks[i-1][:12])
         decrypted_blocks.append(cipher.decrypt(encrypted_blocks[i]))
 
-    # Combine decrypted plaintext blocks
-    plaintext = b''.join(decrypted_blocks) 
-    return plaintext
+    # Combine decrypted pixel blocks
+    decrypted_pixels = b''.join(decrypted_blocks)
+
+    # Create a new PIL image from the decrypted pixels
+    image = Image.frombytes('RGB', (width, height), decrypted_pixels)
+
+    return image
+    # # Split encrypted message into IV and encrypted ciphertext
+    # iv = encrypted_message[:16]
+    # encrypted_ciphertext = encrypted_message[16:]
+
+    # # Divide encrypted ciphertext into blocks of 128 bits
+    # block_size = 16  # 16 bytes = 128 bits
+    # encrypted_blocks = [encrypted_ciphertext[i:i+block_size] for i in range(0, len(encrypted_ciphertext), block_size)]
+
+    # # Use IV and key to decrypt first ciphertext block using AES-128 in CBC mode
+    # cipher = AES.new(key, AES.MODE_CBC, iv)
+    # decrypted_blocks = [cipher.decrypt(encrypted_blocks[0])]
+
+    # # Use decrypted plaintext and key to decrypt subsequent ciphertext blocks using ChaCha20 stream cipher
+    # for i in range(1, len(encrypted_blocks)):
+    #     cipher = ChaCha20.new(key=key, nonce=encrypted_blocks[i-1][:12])
+    #     decrypted_blocks.append(cipher.decrypt(encrypted_blocks[i]))
+
+    # # Combine decrypted plaintext blocks
+    # plaintext = b''.join(decrypted_blocks) 
+    # return plaintext
 
 key = get_random_bytes(32)
 print("*****Image Encryption Using Hybrid AES-128 and ChaCha20 Cipher*****")
 # image_path = "sample1.png"
 # image = Image.open(image_path)
 print("Encrypting Iamge.......")
-img = np.array(Image.open("sample1.png"))
-print("Pixels Array")
-print(img)
+# Open the original image
+original_image_path = "sample1.png"
+image = Image.open(original_image_path)
+
+# Get the width and height of the image
+width, height = image.size
+img = np.array(image)
+# print("Pixels Array")
+# print(img)
 
 to_string = ' '.join(map(str, img.flatten().tolist()))
 print("Pixels")
 print(to_string)
-print("Bytes")
-with open('sample1.png', "rb") as f:
-    image_bytes = f.read() 
-print(image_bytes)
+# print("Bytes")
+# with open('sample1.png', "rb") as f:
+#     image_bytes = f.read() 
+# print(image_bytes)
 
-encrypted_image_bytes = encrypt(to_string, key)
+encrypted_image = encrypt(to_string, key)
  
-encrypted_image_path = "encrypted_image.bin"
-with open(encrypted_image_path, "wb") as f:
-    f.write(encrypted_image_bytes)
- 
+# print("Encrypted")
+# print(encrypted_image)
+# Create a new PIL image from the encrypted pixels
+image = Image.frombytes('RGB', (width, height), encrypted_image)
+
+# Save the encrypted image
+encrypted_image_path = "encrypted_image.png"
+image.save(encrypted_image_path)
+# encrypted_image_path = "encrypted_image.bin"
+# with open(encrypted_image_path, "wb") as f:
+#     f.write(encrypted_image_bytes)
 print("Decrypting Iamge.......")
-  
-decrypted_image_bytes = decrypt(encrypted_image_bytes, key)
+ 
+# Decrypt the encrypted pixels and obtain the decrypted image
+decrypted_image = decrypt(encrypted_image, key, width, height)
 
+# Save the decrypted image
 decrypted_image_path = "decrypted_image.jpg"
-with open(decrypted_image_path, "wb") as f:
-    f.write(decrypted_image_bytes)
+decrypted_image.save(decrypted_image_path) 
+  
+# decrypted_image_bytes = decrypt(encrypted_image, key)
+
+# decrypted_image_path = "decrypted_image.jpg"
+# with open(decrypted_image_path, "wb") as f:
+#     f.write(decrypted_image_bytes)
  
